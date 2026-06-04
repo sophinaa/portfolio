7557c619e08b55dfe4df1233c7ec59214f23dfd9
sophina <115511519+sophinaa@users.noreply.github.com>
2026-06-04 17:46:56 +0100

    about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className='mt-4 mx-auto text-center text-secondary text-[17px] max-w-5xl leading-[30px]'
       >
         Third-year Computer Science student with a growing passion for software
         development and building practical technology that creates real-world
